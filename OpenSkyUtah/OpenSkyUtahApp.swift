402782d45b8a93ca50d3329ca7e0a7c4bae6e392
Koleton Murray <106117059+koletonmurray@users.noreply.github.com>
2024-11-05 08:36:34 -0700

    Initial Commit

---
+++ OpenSkyUtah/OpenSkyUtahApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenSkyUtahApp.swift
+//  OpenSkyUtah
+//
+//  Created by Koleton Murray on 11/5/24.
+//
+
+import SwiftUI
+
+@main
+struct OpenSkyUtahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
